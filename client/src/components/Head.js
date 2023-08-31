11162179362082a1c0f7232fdfaf0d1ef117f448
vivekpanth <vivekpanth09@gmail.com>
2023-08-31 12:01:34 +0530

    used Youtube search API and debouncing

--- client/src/components/Head.js
+++ client/src/components/Head.js
@@ -1,28 +1,66 @@
-import { FaSearch } from "react-icons/fa"; 
-import {useDispatch} from 'react-redux'
-import React from 'react'
+import { FaSearch } from "react-icons/fa";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import React, { useEffect } from "react";
 import { toggleMenu } from "../utils/appSlice";
+import { YOUTUBE_SEARCH_API } from "../utils/contants";
 
 const Head = () => {
-  const dispatch=useDispatch();
-  const toggleMenuHandler=()=>{
-     dispatch(toggleMenu());
-  }
-  return (
-    <div className='grid grid-flow-col p-2 m-1 shadow-lg'>
-      <div className='flex col-span-1'>
-    <img onClick={()=>toggleMenuHandler()} className='h-8 pr-2 cursor-pointer' src="https://paragondigital.com/wp-content/uploads/Menu-Icon2.jpg" alt="menu" />
-    <a href="/"><img className='h-8 pl-2' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCa4EDbkI8ATSXs7s-ovSP2cX_Qfw06aSRWA&usqp=CAU" alt="Logo" /></a>
-    </div>
-    <div className='col-span-10 px-20 flex items-center '>
-      <input className='border-gray-400 border w-1/2 px-5  py-1px rounded-l-full' type="text" />
-      <button className='border-gray-400 border px-5 h-4/5 py-1px rounded-r-full bg-gray-200'><FaSearch /></button>
-    </div>
-    <div className='col-span-1'>
-      <img className='h-8' src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png" alt="user"/>
-    </div>
-    </div>
-  )
-}
+  const [searchQuery, setSearchQuery] = useState("");
+  useEffect(() => {
+    const timer=setTimeout(()=>getSearchSuggestions(),200);
+    return()=>{
+      clearTimeout(timer)
+    }
+  }, [searchQuery]);
 
-export default Head
+  const getSearchSuggestions = async () => {
+    const data = await fetch(`${YOUTUBE_SEARCH_API}${searchQuery}`, { mode: 'no-cors' });
+    const json = await data.json;
+    console.log(json)
+    console.log(YOUTUBE_SEARCH_API + searchQuery + ", { mode: 'no-cors' }")
+  };
+  const dispatch = useDispatch();
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
+  return (
+    <div className="grid grid-flow-col p-2 m-1 shadow-lg">
+      <div className="flex col-span-1">
+        <img
+          onClick={() => toggleMenuHandler()}
+          className="h-8 pr-2 cursor-pointer"
+          src="https://paragondigital.com/wp-content/uploads/Menu-Icon2.jpg"
+          alt="menu"
+        />
+        <a href="/">
+          <img
+            className="h-8 pl-2"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCa4EDbkI8ATSXs7s-ovSP2cX_Qfw06aSRWA&usqp=CAU"
+            alt="Logo"
+          />
+        </a>
+      </div>
+      <div className="col-span-10 px-20 flex items-center ">
+        <input
+          className="border-gray-400 border w-1/2 px-5  py-1px rounded-l-full"
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button className="border-gray-400 border px-5 h-4/5 py-1px rounded-r-full bg-gray-200">
+          <FaSearch />
+        </button>
+      </div>
+      <div className="col-span-1">
+        <img
+          className="h-8"
+          src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+          alt="user"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Head;
--- client/src/utils/contants.js
+++ client/src/utils/contants.js
@@ -1,2 +1,3 @@
 const GOOGLE_API_KEY="AIzaSyAMGke28vt-QZ4tb-4AJoRBctsYP8vgevs"
  export const YOUTUBE_VIDEOS_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+GOOGLE_API_KEY;
+ export const YOUTUBE_SEARCH_API = 'https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q='
